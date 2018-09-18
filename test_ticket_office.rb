35f8fddfd8c1e3030921ab4f29514aa12a7e8fca
Tom <tom.w.page@gmail.com>
2018-09-18 12:56:03 +0200

    First commit

---
+++ test_ticket_office.rb
@@ -0,0 +1,11 @@
+require './ticket_office'
+require './reservations'
+require 'test/unit'
+
+class TestTicketOffice < Test::Unit::TestCase
+
+  def test_reserve_seats
+    # TODO: write this code!
+  end
+
+end
---
+++ ticket_office.rb
@@ -0,0 +1,9 @@
+class TicketOffice
+  
+  def make_reservation(request)
+    # TODO: write this code!
+  end
+end
+
+
+
